e38fb16f7606f2cce90eb7479c008cf179675257
thomstah <thommyxay@gmail.com>
2025-07-10 22:06:25 -0500

    setup

---
+++ app.tsx
@@ -0,0 +1,11 @@
+import './global.css'
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function App() {
+  return (
+    <View>
+      <Text>Welcome to PulseSet!</Text>
+    </View>
+  )
+}
--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,19 @@
 plugins: [
   [
-    "module:react-native-dotenv",
+    'module:react-native-dotenv',
     {
-      moduleName: "@env",
-      path: ".env",
+      moduleName: '@env',
+      path: '.env',
     },
   ],
-];
+]
+
+module.exports = function (api) {
+  api.cache(true)
+  return {
+    presets: [
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+      'nativewind/babel',
+    ],
+  }
+}
